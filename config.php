db06b5ab582ab270da19e05f1a4d5d4e210712ba
asonas <hzw1258@gmail.com>
2011-07-25 05:30:13 +0900

    facebookのアレ追加

--- config.php
+++ config.php
@@ -5,3 +5,9 @@ $config['twitter'] = array(
 	'consumer_secret' => 'uG9PrFwZLNpQbGlWYtIvS6FfWViBPwFTEEntibkBks',
 	'callback_url' => 'http://localhost:8888/oauth/callback.php',
 );
+
+$config['facebook'] = array(
+	'consumer_key' => '232243553473734',
+	'consumer_secret' => '19817113d77d72fdf9f33510f6d27ba6',
+	'callback_url' => 'http://localhost:8888/oauth/callback.php',
+)
