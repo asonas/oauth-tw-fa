5edeefe2239cdcf5c9d5372f3df4bb73ffb28f48
asonas <hzw1258@gmail.com>
2011-07-27 06:17:02 +0900

    コールバック後のURL表示

--- callback-facebook.php
+++ callback-facebook.php
@@ -1,10 +1,19 @@
 <?php
-
+session_start();
 require_once './lib/facebook.php';
 require_once './config.php';
-	$facebook = new Facebook(array(
-		'appId' => $config['facebook']['consumer_key'],
-		'secret' => $config['facebook']['consumer_secret'],
-	));
+
+
+$facebook = new Facebook(array(
+  'appId' => $config['facebook']['consumer_key'],
+  'secret' => $config['facebook']['consumer_secret']
+));
 
 $facebook->api('/me/feed', 'POST', array('message' => $_SESSION['message']));
+
+if( !empty($_SESSION['mixi']['auth_url']) ){
+  echo '<a href="'.$_SESSION['mixi']['auth_url'].'">mixi</a>';
+} elseif(!empty($_SESSION['twitter']['auth_url']) ){
+  echo '<a href="'.$_SESSION['twitter']['auth_url'].'">mixi</a>';
+}
+
