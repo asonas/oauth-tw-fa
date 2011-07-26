287270ff8872eb0373700cf4a49f674776ceee3e
asonas <hzw1258@gmail.com>
2011-07-27 05:33:46 +0900

    modified cakkback_url

--- config.php
+++ config.php
@@ -3,11 +3,11 @@
 $config['twitter'] = array(
 	'consumer_key' => 'pXU3fOcHflvc25uLlrGk8A',
 	'consumer_secret' => 'uG9PrFwZLNpQbGlWYtIvS6FfWViBPwFTEEntibkBks',
-	'callback_url' => 'http://yandona.net/oauth/twitter-callback.php',
+	'callback_url' => 'http://yandona.net/oauth/callback-twitter.php',
 );
 
 $config['facebook'] = array(
 	'consumer_key' => '232243553473734',
 	'consumer_secret' => '19817113d77d72fdf9f33510f6d27ba6',
-	'callback_url' => 'http://yandona.net/oauth/facebook-callback.php',
+	'callback_url' => 'http://yandona.net/oauth/callback-facebook.php',
 );
