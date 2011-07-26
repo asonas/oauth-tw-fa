c919a4594318f4c0991bb030e1bd5844cb6f4acc
asonas <hzw1258@gmail.com>
2011-07-27 05:59:00 +0900

    mod

--- callback-facebook.php
+++ callback-facebook.php
@@ -1,1 +1,10 @@
 <?php
+
+require_once './lib/facebook.php';
+require_once './config.php';
+	$facebook = new Facebook(array(
+		'appId' => $config['facebook']['consumer_key'],
+		'secret' => $config['facebook']['consumer_secret'],
+	));
+
+$facebook->api('/me/feed', 'POST', array('message' => $_SESSION['message']));
