9f68902cb21158e2323b0e2f436c417fa9a98112
asonas <hzw1258@gmail.com>
2011-11-15 01:51:20 +0900

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 
 $config['twitter'] = array(
-	'consumer_key' => 'pXU3fOcHflvc25uLlrGk8A',
-	'consumer_secret' => 'uG9PrFwZLNpQbGlWYtIvS6FfWViBPwFTEEntibkBks',
+	'consumer_key' => '',
+	'consumer_secret' => '',
 	'callback_url' => 'http://yandona.net/oauth/callback-twitter.php',
 );
 
 $config['facebook'] = array(
-	'consumer_key' => '232243553473734',
-	'consumer_secret' => '19817113d77d72fdf9f33510f6d27ba6',
+	'consumer_key' => '',
+	'consumer_secret' => '',
 	'callback_url' => 'http://yandona.net/oauth/callback-facebook.php',
 );
